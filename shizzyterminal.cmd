7e1c9fb73c6ce51c38ad196731106c8a4d0cdde8
Saaim Japanwala <92124191+sjapanwala@users.noreply.github.com>
2023-11-15 18:53:54 -0700

    update 1

--- shizzyterminal.cmd
+++ shizzyterminal.cmd
@@ -1,16 +1,91 @@
 @echo off
 chcp 65001
-mode 60,15
+mode 55,10
 cls                                                                                                                           
-echo [40;35m     _     _               [40;36m _ _               
-echo [40;35m ___│ │__ (_)_________   _ [40;36m(_│_)_________   _ 
-echo [40;35m/ __│ '_ \│ │_  /_  / │ │ │[40;36m│ │ │_  /_  / │ │ │
-echo [40;35m\__ \ │ │ │ │/ / / /│ │_│ │[40;36m│ │ │/ / / /│ │_│ │
-echo [40;35m│___/_│ │_│_/___/___│\__, │[40;36m/ │_/___/___│\__, │
-echo [40;35m                     │___/[40;36m__/           │___/ 
-echo [40;35m                            Loading Terminal...
+echo     [40;35m     _     _               [40;36m _ _               
+echo     [40;35m ___│ │__ (_)_________   _ [40;36m(_│_)_________   _ 
+echo     [40;35m/ __│ '_ \│ │_  /_  / │ │ │[40;36m│ │ │_  /_  / │ │ │
+echo     [40;35m\__ \ │ │ │ │/ / / /│ │_│ │[40;36m│ │ │/ / / /│ │_│ │
+echo     [40;35m│___/_│ │_│_/___/___│\__, │[40;36m/ │_/___/___│\__, │
+echo      [40;35m                     │___/[40;36m__/           │___/ 
+echo.
+timeout 3 >nul
+cls
+echo   [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random%
+timeout 1 >nul
+cls
+echo   [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random%
+echo   [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random%
+timeout 1 >nul
+cls
+echo   [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random%
+echo   [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random%
+echo   [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random%
+timeout 1 >nul
+cls
+echo   [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random%
+echo   [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random%
+echo   [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random%
+echo   [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random%
+timeout 1 >nul
+cls
+echo   [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random%
+echo   [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random%
+echo   [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random%
+echo   [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random%
+echo   [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random%
+timeout 1 >nul
+cls
+echo   [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random%
+echo   [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random%
+echo   [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random%
+echo   [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random%
+echo   [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random%
+echo   [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random%
+timeout 1 >nul
+cls
+echo   [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random%
+echo   [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random%
+echo   [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random%
+echo   [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random%
+echo   [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random%
+echo   [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random%
+echo   [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random%
+timeout 1 >nul
+cls
+echo   [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random%
+echo   [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random%
+echo   [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random%
+echo   [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random%
+echo   [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random%
+echo   [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random%
+echo   [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random%
+echo   [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random%
+timeout 1 >nul
+cls
+echo   [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random%
+echo   [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random%
+echo   [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random%
+echo   [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random%
+echo   [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random%
+echo   [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random%
+echo   [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random%
+echo   [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random%
+echo   [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random%
+timeout 1 >nul
+cls
+echo   [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random%
+echo   [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random%
+echo   [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random%
+echo   [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random%
+echo   [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random%
+echo   [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random%
+echo   [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random%
+echo   [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random%
+echo   [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random%
+echo   [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random% [40;36m%random% [40;35m%random%
 pause
-echo hello
+pause
 
 
 rem start loading section
